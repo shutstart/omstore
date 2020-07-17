6c97237c1414098c9cbbadfcdedffc1ac3423ab0
shutstart <chirag.cool200@gmail.com>
2020-07-17 11:41:00 +0530

    reselect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Switch,Route,Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ import SignInandSignupPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from './components/header/header.component';
 import { auth,createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser }from './redux/user/user.selectors';
 
 class App extends React.Component {
 unsubscribeFromAuth = null
@@ -45,8 +47,8 @@ componentWillUnmount(){
       </div>);
   }
 }
-const mapStateToProps = ({user}) =>({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
