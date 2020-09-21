2327534a244560df27fd72fd640df49bc753f1f2
shutstart <chirag.cool200@gmail.com>
2020-09-21 17:00:39 +0530

    redux 1

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInandSignupPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component'
 import Header from './components/header/header.component';
 import { auth,createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -42,6 +43,7 @@ componentWillUnmount(){
       <Switch>
       <Route exact path='/' component = {HomePage} />
       <Route path='/shop' component = {ShopPage} />
+      <Route exact path ='/checkout' component={CheckoutPage} />
       <Route exact path='/signin' render={()=> this.props.currentUser?(<Redirect to='/' />):(<SignInandSignupPage />)}/>
       </Switch>
       </div>);
