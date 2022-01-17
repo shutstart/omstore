2e6a85c5df65a0d233babd2ad7060a265ee50814
shutstart <chirag.cool200@gmail.com>
2022-01-17 14:32:05 +0530

    updates

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,4 +12,5 @@ if(process.env.NODE_ENV === 'development'){
 export const store = createStore(rootReducer, applyMiddleware(...middlewares))
 export const persistor = persistStore(store);
 
-export default { store, persistor };
+const exp = { store, persistor };
+export default exp;
