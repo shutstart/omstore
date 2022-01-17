f08eaf28ca50e494c19285f05993cb96b996440d
shutstart <chirag.cool200@gmail.com>
2022-01-17 12:33:08 +0530

    updated dependencies

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
 const config = {
     apiKey: "AIzaSyBhfGj_b8v3dlCZEo6DkD1R8eDTJ7OjYnQ",
